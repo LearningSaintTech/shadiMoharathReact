0a3458bd5b20d3905831e814dee2a2a6c5463bed
Prachi Kumari <prachikr585@gmail.com>
2025-04-25 17:31:16 +0530

    Changes

--- src/components/about/HeroSection.jsx
+++ src/components/about/HeroSection.jsx
@@ -3,17 +3,24 @@ import Engagement from "../../assets/images/Engagement.svg";
 
 const HeroSection = () => {
   return (
-    <div className="relative w-full">
-      <img src={Engagement} alt="Engagement" className="w-full h-auto object-cover" />
-      <div className="absolute top-1/2 left-[4vw] md:left-[7.5vw] transform -translate-y-1/2 text-white">
-        <h1 className="text-[11vw] md:text-[120px] font-light" style={{ fontFamily: "Rosarivo" }}>
-          About us
-        </h1>
-        <p className="text-[3vw] md:text-[20px] mt-[1vw] font-light leading-snug">
-          Most Trusted and premium Matrimony Service in the World.
-        </p>
-      </div>
-    </div>
+     <div className="relative w-full h-[50vw] md:h-[21.875vw] ">
+            <img
+              src={Engagement}
+              alt="Engagement"
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute top-1/2 left-[4vw] md:left-[7.5vw] transform -translate-y-1/2 text-white">
+              <h1
+                className="text-[11vw] md:text-[6vw]"
+                style={{ fontFamily: "Rosarivo, serif" }}
+              >
+                About Us
+              </h1>
+              <p className="text-[3vw] md:text-[1vw] mt-[1vw] font-light leading-snug">
+                Most Trusted and premium Matrimony Services in the World.
+              </p>
+            </div>
+          </div>
   );
 };
 
